8c0a0a6c4269124eb07bf08cf9d9361faec40918
zogak <zogak7@gmail.com>
2021-05-02 17:10:27 +0900

    login part

--- stone_community/stoneUser/views.py
+++ stone_community/stoneUser/views.py
@@ -1,19 +1,46 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.contrib.auth.hashers import make_password
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
 from .models import stoneUser
 
 # Create your views here.
+
+def home(request):
+    return HttpResponse('home!')
+    
+def login(request):
+    if request.method == 'GET': #request가 get으로 들어온 경우
+        return render(request, 'login.html') #화면 그냥 보여줌
+    
+    elif request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+        
+        res_data = {}
+        if not (username and password): #값을 모두 입력하지 않았을 시
+            res_data['error'] = '모든 값을 입력해야 합니다.'
+        
+        else:
+            stoneuser = stoneUser.objects.get(username = username)
+            if check_password(stoneuser, stoneUser.password):
+                request.session['user'] = stoneuser.id
+                return redirect('/')
+
+            else: #비번 틀림
+                res_data['error'] = '비밀번호가 틀렸습니다.'
+        return render(request, 'login.html', res_data)
+
 def register(request):
     if request.method == 'GET':
         return render(request, 'register.html')
+    
     elif request.method == 'POST':
         username = request.POST.get('username', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re_password', None)
 
         res_data ={}
-        if not (username and password and re_password) :
+        if not (username and password and re_password):
             res_data['error'] = '모든 값을 입력해야 합니다'
         elif password != re_password:
             res_data['error'] = '비밀번호가 다릅니다'
@@ -24,5 +51,4 @@ def register(request):
             password = make_password(password)
             )
             stoneuser.save()
-
         return render(request, 'register.html', res_data)
